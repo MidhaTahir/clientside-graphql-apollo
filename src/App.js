a8e02be43ed9f7d6d17e0e66de56178fbebcdeb7
MidhaTahir <midhatahirkhan2011@gmail.com>
2021-11-23 17:06:13 +0500

    add: useLazyQuery

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import { Route } from "wouter";
+import { Route, Switch } from "wouter";
 import CharacterLists from "./Pages/CharacterLists";
 import "./App.css";
 import Character from "./Pages/Character";
+import SearchBar from "./Pages/SearchBar";
 
 function App() {
   return (
     <div className="App">
-      <Route path="/">
-        <CharacterLists />
-      </Route>
-
-      <Route path="/:id">
-        <Character />
-      </Route>
+      <Switch>
+        <Route path="/" component={CharacterLists} />
+        <Route path="/search" component={SearchBar} />
+        <Route path="/:id" component={Character} />
+      </Switch>
     </div>
   );
 }
---
+++ src/Pages/SearchBar.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { gql, useLazyQuery } from "@apollo/client";
+
+const GET_CHARACTER_LOCATIONS = gql`
+  query GetCharacterLocations($name: String!) {
+    characters(filter: { name: $name }) {
+      results {
+        location {
+          name
+        }
+      }
+    }
+  }
+`;
+
+const SearchBar = () => {
+  const [name, setName] = useState("");
+  //inside array --> [func we will call to invoke this query , {loading,error,data}]
+  const [getLocations, { loading, error, data, called }] = useLazyQuery(
+    GET_CHARACTER_LOCATIONS,
+    {
+      variables: {
+        name,
+      },
+    }
+  );
+
+  console.log([getLocations, { loading, error, data, called }]);
+
+  return (
+    <div>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={() => getLocations()}>Search</button>
+      {loading && <p>Loadingg..</p>}
+      {error && <p>Something went wrong</p>}
+      {data && (
+        <ul>
+          {data.characters.results.map((character) => {
+            return <li>{character.location.name}</li>;
+          })}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default SearchBar;
